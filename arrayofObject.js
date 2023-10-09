a5bf17b00807ce1d32f2440ccfe90ba2f9233547
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-09 20:04:21 +0530

    array of Objects

--- arrayofObject.js
+++ arrayofObject.js
@@ -1,13 +1,30 @@
 var arrayObj = [{
     name : "Virat Kohli",
     age :"34",
-    country : "India"
+    iplteam : "RCB"
 },{
     name : "Rohit Sharma",
     age : "43",
-    country : "India"
+    iplteam : "Mumbai Indians"
+},{
+    name : "Msd",
+    age : 45,
+    iplteam : "Chennai Super King"
+},{
+    name : "Siraj",
+    age : 28,
+    iplteam : "RCB"
+},{
+    name : "Jadeja",
+    age : 35,
+    iplteam : "Chennai Super King"
+},{
+    name : "Ishan Kishan",
+    age : 25,
+    iplteam : "Mumbai Indians"
 }]
-var ageList = arrayObj.filter((item)=>{
-    return item
+console.log(arrayObj)
+var filterList = arrayObj.filter((item)=>{
+    return item.iplteam == "RCB"
 })
-console.log(arrayObj.age);
+console.log(filterList);
