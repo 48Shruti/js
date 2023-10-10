6b4bd062d622626695b4097e01bb129a39e89927
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-10-10 20:03:53 +0530

    for , while and do while loop

---
+++ loops.js
@@ -0,0 +1,16 @@
+//for loop 
+for(let i = 0 ; i<= 5;i++){
+    console.log("for loop of i",i)
+}
+//while loop
+let a = 0
+while( a<=5){
+ a++
+ console.log("while loop ", a)
+}
+//do while loop
+let b = 0
+do{
+b++
+console.log("do while loop ",b)
+}while( b<=5)
